fe360aef5fb4e9128cb3635f417ff3647cc3cdfd
cxMiguelSilva <miguel.dasilva@checkmarx.com>
2023-01-04 15:48:37 +0000

    add data

---
+++ test/src/test/java/de/widdix/awscftemplates/state/TestRDSAurora.java
@@ -0,0 +1,115 @@
+package de.widdix.awscftemplates.state;
+
+import com.amazonaws.services.cloudformation.model.Parameter;
+import de.widdix.awscftemplates.ACloudFormationTest;
+import de.widdix.awscftemplates.Context;
+import org.junit.Test;
+
+public class TestRDSAurora extends ACloudFormationTest {
+
+    @Test
+    public void testMySQL56() {
+        final Context context = new Context();
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String clientStackName = "client-" + this.random8String();
+        final String stackName = "rds-aurora-" + this.random8String();
+        final String password = this.random8String();
+        try {
+            this.createStack(context, vpcStackName, "vpc/vpc-2azs.yaml");
+            try {
+                this.createStack(context, clientStackName,
+                        "state/client-sg.yaml",
+                        new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName)
+                );
+                try {
+                    this.createStack(context, stackName,
+                            "state/rds-aurora.yaml",
+                            new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                            new Parameter().withParameterKey("ParentClientStack").withParameterValue(clientStackName),
+                            new Parameter().withParameterKey("DBName").withParameterValue("db1"),
+                            new Parameter().withParameterKey("DBMasterUserPassword").withParameterValue(password)
+                    );
+                    // TODO how can we check if this stack works? start a bastion host and try to connect?
+                } finally {
+                    this.deleteStack(context, stackName);
+                }
+            } finally {
+                this.deleteStack(context, clientStackName);
+            }
+        } finally {
+            this.deleteStack(context, vpcStackName);
+        }
+    }
+
+    @Test
+    public void testMySQL57() {
+        final Context context = new Context();
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String clientStackName = "client-" + this.random8String();
+        final String stackName = "rds-aurora-" + this.random8String();
+        final String password = this.random8String();
+        try {
+            this.createStack(context, vpcStackName, "vpc/vpc-2azs.yaml");
+            try {
+                this.createStack(context, clientStackName,
+                        "state/client-sg.yaml",
+                        new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName)
+                );
+                try {
+                    this.createStack(context, stackName,
+                            "state/rds-aurora.yaml",
+                            new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                            new Parameter().withParameterKey("ParentClientStack").withParameterValue(clientStackName),
+                            new Parameter().withParameterKey("Engine").withParameterValue("aurora-mysql"),
+                            new Parameter().withParameterKey("DBName").withParameterValue("db1"),
+                            new Parameter().withParameterKey("DBMasterUserPassword").withParameterValue(password)
+                    );
+                    // TODO how can we check if this stack works? start a bastion host and try to connect?
+                } finally {
+                    this.deleteStack(context, stackName);
+                }
+            } finally {
+                this.deleteStack(context, clientStackName);
+            }
+        } finally {
+            this.deleteStack(context, vpcStackName);
+        }
+    }
+
+    @Test
+    public void testPostgres() {
+        final Context context = new Context();
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String clientStackName = "client-" + this.random8String();
+        final String stackName = "rds-aurora-" + this.random8String();
+        final String password = this.random8String();
+        try {
+            this.createStack(context, vpcStackName, "vpc/vpc-2azs.yaml");
+            try {
+                this.createStack(context, clientStackName,
+                        "state/client-sg.yaml",
+                        new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName)
+                );
+                try {
+                    this.createStack(context, stackName,
+                            "state/rds-aurora.yaml",
+                            new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                            new Parameter().withParameterKey("ParentClientStack").withParameterValue(clientStackName),
+                            new Parameter().withParameterKey("Engine").withParameterValue("aurora-postgresql"),
+                            new Parameter().withParameterKey("DBInstanceClass").withParameterValue("db.r4.large"),
+                            new Parameter().withParameterKey("DBName").withParameterValue("db1"),
+                            new Parameter().withParameterKey("DBMasterUserPassword").withParameterValue(password)
+                    );
+                    // TODO how can we check if this stack works? start a bastion host and try to connect?
+                } finally {
+                    this.deleteStack(context, stackName);
+                }
+            } finally {
+                this.deleteStack(context, clientStackName);
+            }
+        } finally {
+            this.deleteStack(context, vpcStackName);
+        }
+    }
+
+}
